ba19ff99ef28f518878ba324120b18e0df58c7d4
wnsghd14 <wnsghd14@gmail.com>
2022-10-07 11:42:31 +0900

    ADD_1141AM_CRUD기능추가완료_준홍

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,9 @@ app_name = "reviews"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('reviews/', views.reviews, name="reviews"),
-    path('create/', views.create, name="create"),
-
+    path('reviews/', views.reviews, name="home"),
+    path('reviews/create/', views.create, name="create"),
+    path('reviews/<int:pk>', views.detail, name='detail'),
+    path('reviews/<int:pk>/delete', views.delete, name='delete'),
+    path('reviews/<int:pk>/update', views.update, name='update'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -16,10 +16,11 @@ def reviews(request):
 def create(request):
     if request.method == "POST":
         review_form = ReviewForm(request.POST)
+        
         if review_form.is_valid():
             review_form.save()
-            return redirect('reviews:reviews')
-    
+            return redirect('reviews:home')
+
     else:
         review_form = ReviewForm()
     
@@ -27,3 +28,33 @@ def create(request):
         "review_form" : review_form,
     }
     return render(request, "reviews/create.html", context)
+
+def detail(request,pk):
+    review = Review.objects.get(pk=pk)
+
+    context = {
+        'review' : review,
+    }
+    return render(request, 'reviews/detail.html', context)
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    review.delete()
+    return redirect('reviews:home')
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, instance=review)
+        
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('reviews:detail',review.pk)
+
+    else:
+        review_form = ReviewForm(instance=review)
+    
+    context = {
+        "review_form" : review_form,
+    }
+    return render(request, "reviews/update.html", context)
